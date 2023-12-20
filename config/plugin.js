dc17c0f59c0a0b1eb7eec9390785b8e71d6b0715
Jessyelie <jessy.anator@ism.edu.sn>
2023-12-20 14:47:56 +0000

    backend gestion vetement

---
+++ config/plugin.js
@@ -0,0 +1,48 @@
+// config/plugins.js
+const { apolloPrometheusPlugin } = require('strapi-prometheus')
+// enable plugin with default configuration.
+module.exports = [
+  {'strapi-prometheus': {
+    enabled: true,
+    config: {
+      // add prefix to all the prometheus metrics names.
+      prefix: '',
+
+      // use full url instead of matched url
+      // true  => path label: `/api/models/1`
+      // false => path label: `/api/models/:id`
+      fullURL: false,
+
+      // include url query in the url label
+      // true  => path label: `/api/models?limit=1`
+      // false => path label: `/api/models`
+      includeQuery: false,
+
+      // metrics that will be enabled, by default they are all enabled.
+      enabledMetrics: {
+        koa: true, // koa metrics
+        process: true, // metrics regarding the running process
+        http: true, // http metrics like response time and size
+        apollo: true, // metrics regarding graphql
+      },
+
+      // interval at which rate metrics are collected in ms
+      interval: 10_000,
+
+      // set custom/default labels to all the prometheus metrics
+      customLabels: {
+        name: "strapi-prometheus",
+      },
+    },
+    
+  },
+graphql: {
+    enabled: true,
+    config: {
+      apolloServer: {
+        plugins: [apolloPrometheusPlugin], // add the plugin to get apollo metrics
+        tracing: true, // this must be true to get some of the data needed to create the metrics
+      }
+    }
+  }}
+];
